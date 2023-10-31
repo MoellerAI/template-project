9a2cf4485577b6455cd5457e966cfea40d7b31c4
moellerai <mads.moeller@outlook.com>
2023-10-31 16:45:58 +0100

    Added footer.

--- app/frontend/src/app/layout.tsx
+++ app/frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "@/components/ui/header";
+import Footer from "@/components/ui/footer";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 
@@ -14,6 +15,7 @@ function RootLayout({ children }: { children: React.ReactNode }) {
                     <div className="container mx-auto px-4 sm:px-6 lg:px-8 max-w-5xl h-full relative z-10">
                         <div className="my-5">{children}</div>
                     </div>
+                    <Footer />
                     <Toaster />
                 </div>
             </div>
